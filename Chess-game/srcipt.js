19dd22d7a9e6bdc7837a0d73bda192da82962cd9
Sabri <sabri.alperen.kaya@gmail.com>
2023-07-11 08:56:23 +0300

    Chess game update(2)

--- Chess-game/srcipt.js
+++ Chess-game/srcipt.js
@@ -6,17 +6,18 @@ const infoDisplay = document.querySelector('#info-display');
 
 const width = 8;
 
-const tempArr = [];
+let playerGo = 'black';
+playerDisplay.textContent = 'black';
 
 const startPieces = [
-    rook, knight, bishop, queen, knight, bishop, knight, rook,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-    rook, knight, bishop, queen, knight, bishop, knight, rook
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ];
 
 function createBoard(){
@@ -25,7 +26,6 @@ function createBoard(){
         square.classList.add('square');
         square.innerHTML = startPiece;
         square.setAttribute('square-id',i);
-        //square.classList.add('beige');
         const row = Math.floor((63 - i) / 8) + 1;
         if(row % 2 === 0){
             square.classList.add(i % 2 === 0 ? 'beige' : 'brown');
@@ -38,18 +38,20 @@ function createBoard(){
             square.firstChild.classList.add('white');
         }
 
-        square.firstChild && square.firstChild.setAttribute('draggable', true);
+        
+        square.firstChild?.setAttribute('draggable', true)
 
         gameBoard.append(square);
 
         
 
-    });
+    })
+    
 }
 
 createBoard();
 
-const allSquares = document.querySelectorAll("#gameboard .square");
+const allSquares = document.querySelectorAll(".square");
 
 allSquares.forEach((square) => {
     square.addEventListener('dragstart', dragStart);
@@ -57,11 +59,11 @@ allSquares.forEach((square) => {
     square.addEventListener('drop', dragDrop);
 })
 
-let startPosition;
+let startPositionId;
 let draggedElement;
 
 function dragStart(e){
-    startPosition = e.target.parentNode.getAttribute('square-id');
+    startPositionId = e.target.parentNode.getAttribute('square-id');
     draggedElement = e.target;
 }
 
@@ -70,10 +72,149 @@ let lastPosition;
 function dragOver(e){
    e.preventDefault();
 }
-function dragDrop(e){
+
+function dragDrop(e) {
     e.stopPropagation();
+    console.log(draggedElement)
+    const correctGo = draggedElement.classList.contains(playerGo)
 
-    e.target.parentNode.append(draggedElement);
+    const taken = e.target.classList.contains('piece');
 
-    e.target.remove();
+    const opponentGo = playerGo === 'white' ? 'black' : 'white'
+
+    const takenByOpponent = e.target?.classList.contains(opponentGo)
+
+    const valid = checkIfValid(e.target)
+
+    console.log('correct go', correctGo)
+    console.log('taken', taken)
+    console.log('opponent go', opponentGo)
+    console.log('taken by opponent', takenByOpponent)
+    console.log('valid', valid)
+
+    if (correctGo) {
+        if (takenByOpponent && valid) {
+            e.target.parentNode.append(draggedElement);
+            // Önceki konumdan taşı kaldır
+            e.target.remove()
+            changePlayer();
+            return;
+        }
+        // Daha sonra bu kontrolü yap
+        if (taken && !takenByOpponent) {
+            infoDisplay.textContent = "You cannot go here!";
+            setTimeout(() => {
+                infoDisplay.textContent = "";
+            }, 2000);
+            return;
+        }
+        if (valid) {
+            e.target.append(draggedElement);
+            // Önceki konumdan taşı kaldır
+            changePlayer();
+            return;
+        }
+    }
+}
+
+
+
+
+
+
+
+ function checkIfValid(target){
+    const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'))
+
+    const startId = Number(startPositionId)
+
+    const piece = draggedElement.id
+
+    switch(piece){
+        case 'pawn':
+            const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
+
+            if(
+                (starterRow.includes(startId) && (startId + (width * 2)) === targetId) ||
+                (startId + width === targetId) || 
+                (startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild) ||
+                (startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild)
+
+            ){
+                return true;
+            }
+            break;
+        
+        case 'knight':
+            if(
+                startId + width + 2 === targetId || 
+                startId + width - 2 === targetId ||
+                startId - width + 2 === targetId || 
+                startId - width - 2 === targetId ||
+                startId + width*2 + 1 === targetId ||
+                startId + width*2 - 1 === targetId ||
+                startId - width*2 + 1 === targetId ||
+                startId - width*2 - 1 === targetId 
+             ){
+                return true;
+            }
+            break;
+        
+        case 'bishop':
+            if(
+                startId + width + 1 === targetId ||
+
+                startId + width * 2 + 2 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild ||
+
+                startId + width * 3 + 3 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*2 + 2}"]`).firstChild || 
+
+                startId + width * 4 + 4 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*2 + 2}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*3 + 3}"]`).firstChild  ||
+
+                startId + width * 5 + 5 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*2 + 2}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*3 + 3}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*4 + 4}"]`).firstChild  ||
+
+                startId + width * 6 + 6 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*2 + 2}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*3 + 3}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*4 + 4}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*5 + 5}"]`).firstChild  ||
+
+                startId + width * 7 + 7 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*2 + 2}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*3 + 3}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*4 + 4}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*5 + 5}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*6 + 6}"]`).firstChild  ||
+
+                startId + width * 8 + 8 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*2 + 2}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*3 + 3}"]`).firstChild &&
+                !document.querySelector(`[square-id="${startId + width*4 + 4}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*5 + 5}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*6 + 6}"]`).firstChild && !document.querySelector(`[square-id="${startId + width*7 + 7}"]`).firstChild
+
+            ){
+                return true;
+            }
+            break;
+        
+        case 'rook':
+    }
  }
+
+ function changePlayer(){
+    if(playerGo === "black"){
+        reverseIds();
+        playerGo = "white";
+        playerDisplay.textContent = "white"
+    } else {
+        revertIds();
+        playerGo = "black"
+        playerDisplay.textContent = "black"
+    }
+ }
+
+ function reverseIds() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i) => {square.setAttribute('square-id', (width * width - 1) - i)})
+ }
+ function revertIds() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i) => square.setAttribute("square-id", i));
+  }
+  
